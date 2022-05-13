09089fd7c757ca571b49bf3294717615db2440bc
Peter <peter@quantr.hk>
2022-05-13 10:03:47 +0800

    dev

--- src/hooks/useUserInfoAD.ts
+++ src/hooks/useUserInfoAD.ts
@@ -9,10 +9,13 @@ export default function useUserInfoAD() {
     const [peoplePicker, setPeoplePicker] = useState(null);
 
     useEffect(() => {
+        console.log('peoplePicker', peoplePicker);
+
         if (peoplePicker) {
             if (Array.isArray(peoplePicker)) {
                 if (peoplePicker.length > 0) {
                     const email = peoplePicker[0].EMail != undefined ? peoplePicker[0].EMail : peoplePicker[0].secondaryText || peoplePicker[0];
+                    console.log('email', email);
                     getUserAdByGraph(email).then(setUser).catch(console.error);
                 }
             }
@@ -21,6 +24,7 @@ export default function useUserInfoAD() {
         }
     }, [peoplePicker, setUser]);
 
-    return [user, setPeoplePicker, peoplePicker];
+    // return [user, setPeoplePicker, peoplePicker];
+    return [user, setUser, peoplePicker];
 }
 
