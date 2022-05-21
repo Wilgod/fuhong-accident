5a38776f43d536ae30c5418fecc839d369d6474b
wilsonkw123 <waterkw2002@gmail.com>
2022-05-22 01:52:44 +0800

    dev

--- src/hooks/useUserInfoAD.ts
+++ src/hooks/useUserInfoAD.ts
@@ -9,13 +9,10 @@ export default function useUserInfoAD() {
     const [peoplePicker, setPeoplePicker] = useState(null);
 
     useEffect(() => {
-        console.log('peoplePicker', peoplePicker);
-
         if (peoplePicker) {
             if (Array.isArray(peoplePicker)) {
                 if (peoplePicker.length > 0) {
                     const email = peoplePicker[0].EMail != undefined ? peoplePicker[0].EMail : peoplePicker[0].secondaryText || peoplePicker[0];
-                    console.log('email', email);
                     getUserAdByGraph(email).then(setUser).catch(console.error);
                 }
             }
@@ -24,7 +21,6 @@ export default function useUserInfoAD() {
         }
     }, [peoplePicker, setUser]);
 
-    // return [user, setPeoplePicker, peoplePicker];
-    return [user, setUser, peoplePicker];
+    return [user, setPeoplePicker, peoplePicker];
 }
 
